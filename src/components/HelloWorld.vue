45cc3eb7e83adcf0a01455232b8d6a31a0af141a
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2023-09-05 10:23:21 +0800

    [Setup] Test Github Actions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@ const count = ref(0);
 		</button>
 		<p>
 			Edit
-			<code>components/HelloWorld.vue</code> to test HMR
+			<code>components/HelloWorld.vue test</code> to test HMR
 		</p>
 	</div>
 
