6fbd8f61a96c7a31b7a260603ff582047106e957
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2023-09-06 14:06:33 +0800

    [Misc] Setup Routes

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from "vue-router";
+import MainPage from "../views/MainPage.vue";
+import SubPage from "../views/SubPage.vue";
+
+const routes = [
+	{
+		path: "/",
+		name: "Home",
+		component: MainPage,
+	},
+	{
+		path: "/about",
+		name: "About",
+		component: SubPage,
+	},
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
+
+export default router;
