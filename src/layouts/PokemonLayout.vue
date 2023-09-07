3649c0a34f688fa8dc437ef37751e0fb02ab395f
Ian Urriza <ianmichael.urriza@sun-asterisk.com>
2023-09-07 09:52:21 +0800

    [Misc] Setup SCSS

--- src/layouts/PokemonLayout.vue
+++ src/layouts/PokemonLayout.vue
@@ -1,5 +1,15 @@
 <script></script>
 
 <template>
-	<slot></slot>
+	<div class="test">
+		<slot></slot>
+	</div>
 </template>
+
+<style scoped lang="scss">
+div {
+	&.test {
+		color: rgb(100, 0, 0);
+	}
+}
+</style>
