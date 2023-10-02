508aa3a3a49ce0ccb719e9be0e687b8e2a61da35
IanUrriza10 <114897466+IanUrriza10@users.noreply.github.com>
2023-10-02 20:03:32 +0800

    [Markup] Stats Table (#14)

---
+++ src/components/PokemonPage/Stats/StatContainer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import StatTable from "./StatTable.vue";
+</script>
+<template>
+	<div class="container">
+		<StatTable />
+	</div>
+</template>
+<style scoped lang="scss">
+.container {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+</style>
